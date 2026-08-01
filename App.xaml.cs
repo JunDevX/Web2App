38916f73fb0e5f074138bae36e4cd8a2b1d2eed2
JunDevX <sen1x@inbox.ru>
2026-08-02 00:08:24 +0300

    New Design: Fluent Windows 11

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,8 +9,6 @@ namespace Web2AppLauncher
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
-            // Закрывать приложение только когда закрыты ВСЕ его окна
             this.ShutdownMode = ShutdownMode.OnLastWindowClose;
 
             // Если ярлык передал аргументы пути к папке приложения
@@ -19,14 +17,13 @@ namespace Web2AppLauncher
                 string configDir = e.Args[0].Trim('"');
                 if (Directory.Exists(configDir))
                 {
-                    // Запускаем ИСКЛЮЧИТЕЛЬНО окно веб-приложения
                     AppWindow appWin = new AppWindow(configDir);
                     appWin.Show();
                     return;
                 }
             }
 
-            // Если запустили генератор напрямую — открываем ТОЛЬКО генератор
+            // Если запустили генератор напрямую — открываем его
             MainWindow mainWin = new MainWindow();
             mainWin.Show();
         }
